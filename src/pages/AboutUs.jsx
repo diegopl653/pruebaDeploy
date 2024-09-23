0f05fd765099317744132e0a62c175c4ab88d021
Diego Perez <Diego.fernando.perez.luna@gmail.com>
2024-09-23 19:49:01 -0400

    Add: changes

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,13 +1,13 @@
 import { useParams } from "react-router-dom";
 
 const AboutUs = () => {
-  const apiUrl = process.env.APIKEY;
   const { id } = useParams();
 
   return <div>
     <h2>
       About Us
     </h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui tenetur nihil molestias, reiciendis cum minima laudantium rem velit. Fuga, laudantium! Facilis quos ea soluta architecto recusandae illum numquam odit deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae aliquam rem asperiores provident! Repudiandae ducimus delectus ullam pariatur recusandae, quaerat reiciendis! Temporibus eius itaque, rem in vero voluptate facilis nihil? Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a eum corrupti magni quam? Porro eum est quasi sit nostrum? Vero cum nesciunt accusantium laboriosam illo itaque magni hic. Sapiente.</p>
     <p>El id es {id}</p>
   </div>;
 };
